2c3879eb1c7d043c73c3ec89e97f36e151024f04
JeanMarc-RAJAONARIVELONA <jeanmarc.rajaonarivelona01@gmail.com>
2024-03-17 19:01:06 +0300

    style: create design of side bar who contain link

--- src/component/aside/Aside.jsx
+++ src/component/aside/Aside.jsx
@@ -1,29 +1,53 @@
 import React from "react";
+import "./Aside.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faWallet,
+  faMoneyCheck,
+  faMoneyBillTransfer,
+  faHandHoldingDollar,
+  faGear,
+  faRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 function Aside() {
   return (
-    <section
-      style={{
-        position: "fixed",
-        top: "0",
-        left: "0",
-        width: "15vw",
-        height: "100vh",
-        background: "blue",
-      }}
-    >
+    <section className="aside">
       <h1>U-bank</h1>
       <div className="link">
-        <ul>
-          <li>Dashboard</li>
-          <li>Accounts</li>
-          <li>Currency</li>
-          <li>Transaction</li>
-          <li>Balance</li>
-        </ul>
-        <ul>
-          <li>Settings</li>
-          <li>Log out</li>
-        </ul>
+        <div className="items">
+          <Link href="/dashboard" className="link-items">
+            <FontAwesomeIcon icon={faHouse} className="icon" />
+            Dashboard
+          </Link>
+          <Link href="/accounts" className="link-items">
+            <FontAwesomeIcon icon={faWallet} className="icon" />
+            Accounts
+          </Link>
+          <Link href="/currency" className="link-items">
+            <FontAwesomeIcon icon={faMoneyCheck} className="icon" />
+            Currency
+          </Link>
+          <Link href="/transaction" className="link-items">
+            <FontAwesomeIcon icon={faMoneyBillTransfer} className="icon" />
+            Transaction
+          </Link>
+          <Link href="/balance" className="link-items">
+            <FontAwesomeIcon icon={faHandHoldingDollar} className="icon" />
+            Balance
+          </Link>
+        </div>
+        <div className="items">
+          <Link href="/setting" className="link-items">
+            <FontAwesomeIcon icon={faGear} className="icon" />
+            Settings
+          </Link>
+          <Link href="login" className="link-items">
+            <FontAwesomeIcon icon={faRightFromBracket} className="icon" />
+            Log out
+          </Link>
+        </div>
       </div>
     </section>
   );
